51840634ea2d761aa16011b88e9afbda08ab0fb0
magenbluten <mb@64k.by>
2020-04-27 11:43:43 +0000

    clear offets after each query, sort index by filenames ( should be ok cz of the leading zeros )

--- nygma/nygma/ny-command-offset-by.cxx
+++ nygma/nygma/ny-command-offset-by.cxx
@@ -13,9 +13,9 @@
 #include <map>
 
 extern "C" {
-#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
 }
 
 namespace nygma {
@@ -51,6 +51,12 @@ void index_file_dependencies::gather(
       _iy.push_back( p );
     }
   }
+
+  std::sort( _i4.begin(), _i4.end() );
+  std::sort( _i6.begin(), _i6.end() );
+  std::sort( _ix.begin(), _ix.end() );
+  std::sort( _iy.begin(), _iy.end() );
+
   flog( lvl::i, "index_file_dependencies._count_i4 = ", _i4.size() );
   flog( lvl::i, "index_file_dependencies._count_i6 = ", _i6.size() );
   flog( lvl::i, "index_file_dependencies._count_ix = ", _ix.size() );
@@ -75,48 +81,56 @@ void ny_command_offset_by( offsets_by_config const& config ) {
 
   if( not config._key_i4.empty() ) {
     auto const key = ntohl( ::inet_addr( config._key_i4.c_str() ) );
-    flog( lvl::i, "executing query = i4(", config._key_i4, ") (", key, ")" );
+    flog( lvl::i, "executing query = i4( ", config._key_i4, " ) ( ", key, " )" );
     for( auto& p : deps._i4 ) {
+      flog( lvl::i, "executing query on index file = ", p );
+      offsets.clear();
       auto iv = riot::make_poly_index_view( p );
+      std::cout << "@segment offset = " << iv->segment_offset() << std::endl;
       iv->query( key, offsets );
-      std::cout << "segment offset = " << iv->segment_offset() << std::endl;
       std::ostream_iterator<std::uint32_t> out{ std::cout, "\n" };
       std::copy_n( offsets.begin(), std::min( offsets.size(), per_index_limit ), out );
     }
   }
   if( not config._key_i6.empty() ) {
     __uint128_t key;
-    if( auto const rc = ::inet_pton(AF_INET6, config._key_i6.c_str(), &key); rc <= 0) {
+    if( auto const rc = ::inet_pton( AF_INET6, config._key_i6.c_str(), &key ); rc <= 0 ) {
       flog( lvl::e, "invalid i6 key = ", config._key_i6 );
-      throw std::runtime_error( "invalid i6 key");
+      throw std::runtime_error( "invalid i6 key" );
     }
-    flog( lvl::i, "executing query = i6(", config._key_i6, ")");
+    flog( lvl::i, "executing query = i6( ", config._key_i6, " )" );
     for( auto& p : deps._i6 ) {
+      flog( lvl::i, "executing query on index file = ", p );
+      offsets.clear();
       auto iv = riot::make_poly_index_view( p );
+      std::cout << "@segment offset = " << iv->segment_offset() << std::endl;
       iv->query_ex( key, offsets );
-      std::cout << "segment offset = " << iv->segment_offset() << std::endl;
       std::ostream_iterator<std::uint32_t> out{ std::cout, "\n" };
       std::copy_n( offsets.begin(), std::min( offsets.size(), per_index_limit ), out );
     }
   }
   if( not config._key_ix.empty() ) {
     auto const key = static_cast<std::uint32_t>( std::stoul( config._key_ix ) );
-    flog( lvl::i, "executing query = ix(", config._key_ix, ") (", key, ")" );
+    flog( lvl::i, "executing query = ix( ", config._key_ix, " ) ( ", key, " )" );
     for( auto& p : deps._ix ) {
+      flog( lvl::i, "executing query on index file = ", p );
+      offsets.clear();
       auto iv = riot::make_poly_index_view( p );
+      std::cout << "@segment offset = " << iv->segment_offset() << std::endl;
       iv->query( key, offsets );
-      std::cout << "segment offset = " << iv->segment_offset() << std::endl;
       std::ostream_iterator<std::uint32_t> out{ std::cout, "\n" };
       std::copy_n( offsets.begin(), std::min( offsets.size(), per_index_limit ), out );
     }
   }
   if( not config._key_iy.empty() ) {
     auto const key = static_cast<std::uint32_t>( std::stoul( config._key_iy ) );
-    flog( lvl::i, "executing query = iy(", config._key_iy, ") (", key, ")" );
+    flog( lvl::i, "executing query = iy( ", config._key_iy, " ) ( ", key, " )" );
     for( auto& p : deps._iy ) {
+      flog( lvl::i, "executing query on index file = ", p );
+      offsets.clear();
       auto iv = riot::make_poly_index_view( p );
+      std::cout << "@segment offset = " << iv->segment_offset() << std::endl;
       iv->query( key, offsets );
-      std::cout << "segment offset = " << iv->segment_offset() << std::endl;
       std::ostream_iterator<std::uint32_t> out{ std::cout, "\n" };
       std::copy_n( offsets.begin(), std::min( offsets.size(), per_index_limit ), out );
     }
