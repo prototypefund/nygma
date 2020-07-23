90c0432e11b36d7383a26d90491469d8a4c9ab95
mb <mb@64k.by>
2020-07-23 10:09:23 +0200

    fuzzing: fix typo

--- fuzzing/index-view.driver.cxx
+++ fuzzing/index-view.driver.cxx
@@ -63,7 +63,7 @@ extern "C" int LLVMFuzzerTestOneInput( uint8_t const* p, size_t const sz ) {
     auto const bs = bytestring_view{ reinterpret_cast<std::byte const*>( p ), sz };
     auto const index = riot::make_poly_index_view( bs );
     auto const v4 = "149.171.126.16";
-    auto const ip = __builtin__bswap32( ::inet_addr( v4 ) );
+    auto const ip = __builtin_bswap32( ::inet_addr( v4 ) );
     auto const a = index->lookup_forward_32( ip );
     auto const b = index->lookup_forward_32( 80 );
     auto const c = index->lookup_forward_64( static_cast<std::uint64_t>( ip ) );
